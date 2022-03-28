b5da4f18c3c7773a2c70b144b8e9e00611ccfb69
BacLuc <lucius.bachmann@gmx.ch>
2022-03-28 12:45:22 +0200

    PalindromeTwoTest: add values to cover branches of PalindromeTwo
    For that you need to delete PalindromeOne, the test and the config, because andy cannot handle 2 files under test.

--- assignment1/palindrome/src/test/java/zest/PalindromeTwoTest.java
+++ assignment1/palindrome/src/test/java/zest/PalindromeTwoTest.java
@@ -7,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static zest.PalindromeTwo.isPalindrome;
 
 class PalindromeTwoTest {
-
   @ParameterizedTest
   @CsvSource({
     "121, true",
@@ -16,7 +15,18 @@ class PalindromeTwoTest {
     "0, true",
     "9, true",
     "-9, false",
+    "10, false",
+    "11, true",
     "33, true",
+    "99, true",
+    "98, false",
+    "100, false",
+    "101, true",
+    "110, false",
+    "121, true",
+    "999, true",
+    "1000, false",
+    "1001, true",
     "2332, true",
     "2331, false",
     "14541, true",
