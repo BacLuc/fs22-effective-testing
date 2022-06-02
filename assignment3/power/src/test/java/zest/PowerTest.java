4efa8bdf129b29dda1b76328b7c7cf9666cadbb6
BacLuc <lucius.bachmann@gmx.ch>
2022-06-02 10:10:14 +0200

    PowerTest: add structural tests

--- assignment3/power/src/test/java/zest/PowerTest.java
+++ assignment3/power/src/test/java/zest/PowerTest.java
@@ -4,6 +4,9 @@ import net.jqwik.api.*;
 import net.jqwik.api.lifecycle.BeforeProperty;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +34,39 @@ class PowerTest {
     @Provide
     private Arbitrary<Integer> powers() {
         return Arbitraries.integers().between(-99, 99);
+    }
+
+    @Test
+    void return_1_if_power_is_0() {
+        assertThat(powerCalculator.myPow(5, 0)).isEqualTo(1);
+        assertThat(powerCalculator.myPow(0, 0)).isEqualTo(1);
+        assertThat(powerCalculator.myPow(1, 0)).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5.0, -2",
+            "5.0, 2",
+            "5.0, 4",
+            "5.0, 5",
+            "1.0, -2",
+            "1.0, 2",
+            "1.0, 4",
+            "1.0, 5",
+            "0.0, -2",
+            "0.0, 2",
+            "0.0, 4",
+            "0.0, 5",
+            "-1.0, -2",
+            "-1.0, 2",
+            "-1.0, 4",
+            "-1.0, 5",
+            "-5.0, -2",
+            "-5.0, 2",
+            "-5.0, 4",
+            "-5.0, 5",
+    })
+    void calculate_correct_powers(double base, int power) {
+        assertThat(powerCalculator.myPow(base, power)).isCloseTo(Math.pow(base, power), ERROR_MARGIN);
     }
 }
